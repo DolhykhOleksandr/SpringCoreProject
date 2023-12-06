f281b37a4140df6fd5aae4700a53a10ff7c258a3
Dolhykh Oleksandr <dolhykhaleksandr@gmail.com>
2023-12-07 01:50:00 +0200

    change sql file

--- src/main/resources/DataBase.sql
+++ src/main/resources/DataBase.sql
@@ -1,39 +1,44 @@
 CREATE TABLE IF NOT EXISTS public."order"
 (
-    id   integer          NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1 ),
-    date  timestamp DEFAULT CURRENT_TIMESTAMP NOT NULL,
-    cost double precision NOT NULL,
-    CONSTRAINT order_pkey PRIMARY KEY (id)
+    id   serial PRIMARY KEY,
+    date timestamp DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    cost double precision                    NOT NULL
 );
-
 
 
 CREATE TABLE IF NOT EXISTS public.product
 (
-    id   integer                                            NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1 ),
+    id   serial PRIMARY KEY,
     name character varying(16) COLLATE pg_catalog."default" NOT NULL,
-    cost double precision                                   NOT NULL,
-    CONSTRAINT product_pkey PRIMARY KEY (id)
+    cost double precision                                   NOT NULL
 );
-
 
 
 CREATE TABLE IF NOT EXISTS public.order_product
 (
+    id         serial PRIMARY KEY,
     order_id   integer NOT NULL,
     product_id integer NOT NULL,
-    CONSTRAINT order_product_pkey PRIMARY KEY (order_id, product_id),
-    CONSTRAINT order_fk FOREIGN KEY (order_id)
+    CONSTRAINT order_product_order_fk FOREIGN KEY (order_id)
         REFERENCES public."order" (id) MATCH SIMPLE
         ON UPDATE CASCADE
         ON DELETE CASCADE,
 
-    CONSTRAINT product_fk FOREIGN KEY (product_id)
+    CONSTRAINT order_product_product_fk FOREIGN KEY (product_id)
         REFERENCES public.product (id) MATCH SIMPLE
         ON UPDATE CASCADE
         ON DELETE CASCADE
 );
 
-INSERT INTO public."order" (cost) VALUES (100.00);
-INSERT INTO public.product (name, cost) VALUES ('Product1', 50.00);
-INSERT INTO public.order_product (order_id, product_id) VALUES (1, 1);
+
+INSERT INTO public."order" (date, cost)
+VALUES (CURRENT_TIMESTAMP, 100.0);
+
+
+INSERT INTO public.product (name, cost)
+VALUES ('Product 1', 50.0);
+
+
+INSERT INTO public.order_product (order_id, product_id)
+VALUES (currval(pg_get_serial_sequence('"order"', 'id')), currval(pg_get_serial_sequence('product', 'id')));
+
