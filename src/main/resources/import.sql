7f232e4678a07bb86d9788fce1290775bc421672
Dolhykh Oleksandr <dolhykhaleksandr@gmail.com>
2024-01-11 13:17:50 +0200

    minor changes

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -4,10 +4,10 @@ insert into role (role_id, role_name) values (1,'ADMIN'), (2, 'ADMINTRAINEE');
 
 insert into role_permission (role_id, permission_id) values (1,1), (1,2), (2,1);
 
-insert into user_t (username, password, first_name, last_name, email, locked, disabled) values ('linda', '$2a$10$eRfSRbe1iHM37FX93nr9z.hLJ.R6LkZGGcoTCoq11zZiRHKymd3K2', 'Linda', 'Jones', 'linda@gmail.com', false, false);
+insert into user_t (username, password, first_name, last_name, email, locked, disabled) values ('anton', '$2a$10$eRfSRbe1iHM37FX93nr9z.hLJ.R6LkZGGcoTCoq11zZiRHKymd3K2', 'Anton', 'Shevchenko', 'anton@gmail.com', false, false);
 
-insert into user_t (username, password, first_name, last_name, email, locked, disabled) values ('tom', '$2a$10$eRfSRbe1iHM37FX93nr9z.hLJ.R6LkZGGcoTCoq11zZiRHKymd3K2', 'Tom', 'Banderas', 'tom@gmail.com', false, false);
+insert into user_t (username, password, first_name, last_name, email, locked, disabled) values ('mike', '$$2a$10$eRfSRbe1iHM37FX93nr9z.hLJ.R6LkZGGcoTCoq11zZiRHKymd3K2', 'Mike', 'Malen', 'mike@gmail.com', false, false);
 
-insert into user_role (username, role_id) values ('linda', 1);
+insert into user_role (username, role_id) values ('anton', 1);
 
-insert into user_role (username, role_id) values ('tom', 2);
+insert into user_role (username, role_id) values ('mike', 2);
